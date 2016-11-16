821423028882d4e1aad20d8282cf3ea5a1b208e8
ollieshmollie <oliverduncan@icloud.com>
2016-11-16 16:41:31 -0600

    Add RestaurantController and implement new, create, show, and index methods

---
+++ app/controllers/restaurant_controller.rb
@@ -0,0 +1,24 @@
+class RestaurantController < ApplicationController
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new[restaurant_params]
+    @user.save
+    redirect_to @user
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def index
+    @restaurant = Restaurant.all
+  end
+
+  private
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :city, :state)
+    end
+end
