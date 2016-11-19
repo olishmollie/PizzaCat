658723fccb137e9e467c7698ce20a8f0bdc857d4
ollieshmollie <oliverduncan@icloud.com>
2016-11-19 16:46:23 -0600

    Implement basic encryption scheme

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ApplicationRecord
+  require('modules/encryption.rb')
+  include Encryption
   has_many :ratings
   has_many :restaurants, through: :ratings
+  before_save :encrypt_password
 
   def authenticate(password)
-    password == self.password
+    password == decrypt(self.password)
+  end
+
+  def encrypt_password
+    self.password = encrypt(self.password)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116181046) do
+ActiveRecord::Schema.define(version: 20161117140420) do
 
   create_table "ratings", force: :cascade do |t|
     t.integer  "stars",         null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20161116181046) do
     t.string   "password",   null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "email"
   end
 
 end
