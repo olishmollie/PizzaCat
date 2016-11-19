c00c62ddc0e2394c969a94139667a178eb434989
ollieshmollie <oliverduncan@icloud.com>
2016-11-19 17:02:20 -0600

    Find users by email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(username: params[:username])
+    user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to '/ratings'
     else
-      redirect_to '/login'
+      # redirect_to '/login'
+      render plain: "Unable to log in."
     end
   end
 
